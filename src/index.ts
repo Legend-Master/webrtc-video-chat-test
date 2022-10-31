4f2f0370cb42425195ca2fed18c5e5bc36a14ade
Tony <legendmastertony@gmail.com>
2022-10-31 17:06:46 +0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,122 @@
+const video = document.getElementById('remote-video') as HTMLVideoElement
+const offerBtn = document.getElementById('offer-button') as HTMLButtonElement
+const answerBtn = document.getElementById('answer-button') as HTMLButtonElement
+const answerTextarea = document.getElementById('answer-textarea') as HTMLTextAreaElement
+const copyBtn = document.getElementById('copy-button') as HTMLButtonElement
+const iceGatherState = document.getElementById('ice-gather-state') as HTMLSpanElement
+const iceOutEl = document.getElementById('ice-out') as HTMLDivElement
+const descOutEl = document.getElementById('desc-out') as HTMLDivElement
+
+type MixedData = {
+	ice: RTCIceCandidateInit[]
+	desc: RTCSessionDescriptionInit
+}
+
+const config: RTCConfiguration = {
+	iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }],
+}
+
+let pc: RTCPeerConnection
+let peerType: 'offer' | 'answer' = 'answer'
+
+const icecandidateData: RTCIceCandidateInit[] = []
+let offerDesc: RTCSessionDescriptionInit
+let answerDesc: RTCSessionDescriptionInit
+
+offerBtn.addEventListener('click', async (ev) => {
+	offerBtn.hidden = true
+	peerType = 'offer'
+	await createOfferPeer()
+})
+answerBtn.addEventListener('click', async (ev) => {
+	const userInput = answerTextarea.value
+	if (!userInput) {
+		return
+	}
+	offerBtn.hidden = true
+	answerBtn.hidden = true
+	answerTextarea.hidden = true
+	const data: MixedData = JSON.parse(userInput)
+	if (peerType === 'answer') {
+		offerDesc = data.desc
+		await createAnswerPeer()
+	} else {
+		await pc.setRemoteDescription(data.desc)
+	}
+	for (const candidate of data.ice) {
+		await pc.addIceCandidate(candidate)
+	}
+})
+copyBtn.addEventListener('click', (ev) => {
+	const data: MixedData = {
+		ice: icecandidateData,
+		desc: peerType === 'offer' ? offerDesc : answerDesc,
+	}
+	navigator.clipboard.writeText(JSON.stringify(data))
+})
+
+function registerIceChange() {
+	pc.addEventListener('icecandidate', (ev) => {
+		if (ev.candidate) {
+			// console.log(ev.candidate.candidate)
+			icecandidateData.push(ev.candidate.toJSON())
+			iceOutEl.innerText = JSON.stringify(icecandidateData)
+		}
+	})
+	pc.addEventListener('icegatheringstatechange', (ev) => {
+		// if (pc.iceGatheringState === 'complete') {
+		// 	iceOutEl.innerText = JSON.stringify(icecandidateData)
+		// }
+		iceGatherState.innerText = ` (${pc.iceGatheringState})`
+	})
+}
+
+function registerTrackChange() {
+	pc.addEventListener('track', (ev) => {
+		if (ev.streams[0]) {
+			video.srcObject = ev.streams[0]
+		}
+	})
+}
+
+async function createOfferPeer() {
+	pc = new RTCPeerConnection(config)
+	registerIceChange()
+	registerTrackChange()
+	await addMedia()
+	offerDesc = await pc.createOffer()
+	descOutEl.innerText = JSON.stringify(offerDesc)
+	await pc.setLocalDescription(offerDesc)
+}
+
+async function createAnswerPeer() {
+	pc = new RTCPeerConnection(config)
+	registerIceChange()
+	registerTrackChange()
+	await addMedia()
+	await pc.setRemoteDescription(offerDesc)
+	answerDesc = await pc.createAnswer()
+	descOutEl.innerText = JSON.stringify(answerDesc)
+	await pc.setLocalDescription(answerDesc)
+}
+
+async function addMedia() {
+	const stream = await navigator.mediaDevices.getUserMedia({
+		video: {
+			frameRate: {
+				ideal: 60,
+			},
+			width: {
+				ideal: 1920,
+			},
+			height: {
+				ideal: 1080,
+			},
+		},
+		// audio: true,
+	})
+
+	for (const track of stream.getTracks()) {
+		pc.addTrack(track, stream)
+	}
+}
