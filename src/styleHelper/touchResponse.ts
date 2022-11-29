b6ce38d5f8f526f1b6fb96f1c5623fc0ab3b9fb8
Tony <legendmastertony@gmail.com>
2022-11-29 23:52:42 +0800

    Adjust style for touch screen

--- src/styleHelper/touchResponse.ts
+++ src/styleHelper/touchResponse.ts
@@ -1,20 +1,20 @@
-const touchResponse = document.getElementsByClassName(
-	'touch-response'
-) as HTMLCollectionOf<HTMLElement>
+const elements = [
+	document.getElementsByClassName('touch-response') as HTMLCollectionOf<HTMLElement>,
+	document.getElementsByTagName('button'),
+]
 
-for (const el of touchResponse) {
-	el.addEventListener(
-		'touchstart',
-		() => {
-			el.classList.add('ontouch')
-		},
-		{ passive: true }
-	)
-	el.addEventListener(
-		'touchend',
-		() => {
-			el.classList.remove('ontouch')
-		},
-		{ passive: true }
-	)
+function addTouchClass(this: HTMLElement) {
+	this.classList.add('ontouch')
+}
+
+function removeTouchClass(this: HTMLElement) {
+	this.classList.remove('ontouch')
+}
+
+for (const group of elements) {
+	for (const el of group) {
+		el.addEventListener('touchstart', addTouchClass, { passive: true })
+		el.addEventListener('touchend', removeTouchClass, { passive: true })
+		el.addEventListener('touchmove', removeTouchClass, { passive: true })
+	}
 }
