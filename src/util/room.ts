002f75a0bc23c22d5b30061e8e2842dfdd106eb4
Tony <legendmastertony@gmail.com>
2023-12-17 20:03:44 +0800

    Store recently used rooms

--- src/util/room.ts
+++ src/util/room.ts
@@ -2,9 +2,15 @@ import { push, ref } from 'firebase/database'
 import { db } from './firebaseInit'
 
 const DB_PATH = 'multi-room'
+const RECENTLY_USED_ROOMS_SAVE_KEY = 'recently-used-rooms'
+
+const MAX_RECENTLY_USED_ROOMS = 6
 
 export let roomId: string
 export let room: string
+
+const saved = localStorage.getItem(RECENTLY_USED_ROOMS_SAVE_KEY)
+export const recentlyUsedRooms: string[] = saved ? JSON.parse(saved) : []
 
 const searchParams = new URLSearchParams(location.search)
 const roomParam = searchParams.get('room')
@@ -19,7 +25,7 @@ export function createRoom() {
 			setRoom(id)
 			history.pushState(null, '', `?room=${roomId}`)
 		} else {
-			throw new Error("Can't get a new unique room id");
+			throw new Error("Can't get a new unique room id")
 		}
 	}
 	return roomId
@@ -28,4 +34,25 @@ export function createRoom() {
 function setRoom(id: string) {
 	roomId = id
 	room = `${DB_PATH}/${roomId}`
+	addRecentlyUsedRoom()
+}
+
+function addRecentlyUsedRoom() {
+	for (const [i, id] of recentlyUsedRooms.entries()) {
+		if (id === roomId) {
+			recentlyUsedRooms.splice(i, 1)
+			recentlyUsedRooms.unshift(id)
+			saveRecentlyUsedRooms()
+			return
+		}
+	}
+	recentlyUsedRooms.unshift(roomId)
+	if (recentlyUsedRooms.length > MAX_RECENTLY_USED_ROOMS) {
+		recentlyUsedRooms.pop()
+	}
+	saveRecentlyUsedRooms()
+}
+
+function saveRecentlyUsedRooms() {
+	localStorage.setItem(RECENTLY_USED_ROOMS_SAVE_KEY, JSON.stringify(recentlyUsedRooms))
 }
