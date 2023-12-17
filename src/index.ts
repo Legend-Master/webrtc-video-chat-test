eae2baa20f9b718144a609e621e2503c4454b1d8
Tony <legendmastertony@gmail.com>
2023-12-17 22:32:40 +0800

    Add recent room component

--- src/index.ts
+++ src/index.ts
@@ -5,53 +5,11 @@ import './styleHelper/dialog'
 
 import './custom-video'
 import './floating-video'
+import './recent-room'
 
 import './iceServerData'
 import './selectDevice'
 import './peerConnection'
 import './keyBoardControls'
 
-import { createRoom } from './room'
-import { startPeerConnection } from './peerConnectionsManager'
-import { setVideoState, welcomeDone } from './selectDevice'
-
-const startBtn = document.getElementById('start-button') as HTMLButtonElement
-
-const hiddenAfterCall = document.getElementsByClassName(
-	'hidden-after-call'
-) as HTMLCollectionOf<HTMLElement>
-const shownAfterCall = document.getElementsByClassName(
-	'shown-after-call'
-) as HTMLCollectionOf<HTMLElement>
-
-async function updateHideStyle() {
-	for (const el of hiddenAfterCall) {
-		el.hidden = true
-	}
-	for (const el of shownAfterCall) {
-		el.hidden = false
-	}
-}
-
-function start() {
-	if (document.startViewTransition) {
-		document.startViewTransition(updateHideStyle)
-	} else {
-		updateHideStyle()
-	}
-
-	createRoom()
-	startPeerConnection()
-}
-startBtn.addEventListener('click', start)
-
-async function handleAutoStart() {
-	const searchParams = new URLSearchParams(location.search)
-	const shouldAutoStart = searchParams.get('auto-start') === 'true'
-	if (shouldAutoStart) {
-		setVideoState(false)
-		await welcomeDone
-		start()
-	}
-}
-handleAutoStart()
+import './startHandler'
---
+++ src/recent-room.ts
@@ -0,0 +1,61 @@
+import { start } from './startHandler'
+import mdiPin from 'iconify-icon:mdi/pin'
+
+import './recent-room.css'
+
+class RecentRoom extends HTMLElement {
+	private startButton!: HTMLButtonElement
+	private pinButton!: HTMLButtonElement
+	private separator!: HTMLDivElement
+	private pinned = false
+
+	constructor() {
+		super()
+	}
+
+	connectedCallback() {
+		this.startButton = document.createElement('button')
+		this.startButton.classList.add('start-button')
+		this.startButton.addEventListener('click', () => start())
+
+		this.pinButton = document.createElement('button')
+		this.pinButton.innerHTML = mdiPin
+		this.pinButton.classList.add('pin-button')
+		this.pinButton.addEventListener('click', () => this.togglePinned())
+
+		this.separator = document.createElement('div')
+		this.separator.classList.add('separator')
+
+		this.append(this.startButton, this.separator, this.pinButton)
+	}
+
+	disconnectedCallback() {}
+
+	setRoomId(roomId: string) {
+		this.startButton.innerText = roomId
+	}
+
+	setPinned(pinned: boolean) {
+		this.pinned = pinned
+		this.updatePinnedStyle()
+	}
+
+	togglePinned() {
+		this.pinned = !this.pinned
+		this.updatePinnedStyle()
+	}
+
+	private updatePinnedStyle() {
+		if (this.pinned) {
+			this.classList.add('pinned')
+		} else {
+			this.classList.remove('pinned')
+		}
+	}
+}
+customElements.define('recent-room', RecentRoom)
+declare global {
+	interface HTMLElementTagNameMap {
+		'recent-room': RecentRoom
+	}
+}
